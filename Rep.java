717582113e7aaf5ab43f04b3fe13a7c4cdd42081
Konrad Putelbergier <konrad.putelbergier@gmail.com>
2017-03-07 11:19:01 +0100

    Poprawka dzialania programu

--- Rep.java
+++ Rep.java
@@ -3,30 +3,30 @@ public class Rep {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		int [] arr = {-3, 4, 0, 4, 10, 14, 1, 0, 7, 7, 0};
+
+		int[] arr = {-3, 4, 0, 4, 10, 14, 1, 0, 7, 7, 0};
 		int regular = 0;
 		int max = 0;
 		int counter = 0;
-		
+
 		for (int i = 0; i < arr.length; i++) {
 			counter = 0;
-			for (int j = 0; j <arr.length;j++) {
-				
+			for (int j = 0; j < arr.length; j++) {
+
 				if (arr[i] == arr[j]) {
-				counter++;
-					if (counter>max) {
+					counter++;
+					if (counter > max) {
 						regular = arr[i];
 						max = counter;
 					}
+				}
+
+
 			}
-				
-			
+
+			System.out.println("Najczesciej w tablicy wystepuje: " + regular);
 		}
-	
-			System.out.println("Najczesciej w tablicy wystepuje: " +regular);
 	}
-}
 }
 
 
